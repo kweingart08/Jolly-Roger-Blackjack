381f0d2fbe295f127491bffca3d61f005658854c
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-06-19 12:40:02 -0400

    working code with boats moving, restart button functioning

--- js/app.js
+++ js/app.js
@@ -295,24 +295,4 @@ $(() => { //start of on ready function
   $("#restart").on("click", restart);
 
 
-
-
-  //when you click Let's play button, deal in this order: 1 card facing up in player, 1 card face down in dealer, 1 card facing up in player, and 1 card face up in dealer
-
-
-  //after first hand is dealt
-  //first check if the player has 21 --->
-  //if they don't have 21 -->
-  //options for player:
-  //HIT (if they click the hit button):
-  //deal another card face up , then check if 21 again
-
-  //STAND
-  //flip over the dealers card , if less than 17, deal another one, check if over or above 17 .
-  //check who is closer to 21.
-
-
-
-
-
 }); //end of on ready function
